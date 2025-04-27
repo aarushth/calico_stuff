2499ddafd1e0366fc706156d4af6bb39d7c96f95
aarushth <65953492+aarushth@users.noreply.github.com>
2025-04-26 18:30:25 -0700

    Update 6_stick_drift.cpp

--- 6_stick_drift.cpp
+++ 6_stick_drift.cpp
@@ -1,100 +1,103 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <string>
-#include <algorithm>
-
+#include <cmath>
 using namespace std;
 
-int T;
+int N, M;
 
-pair<int, int> add(pair<int, int> tup1, pair<int, int> tup2, int N, int M) {
-    return make_pair((tup1.first + tup2.first) % N, (tup1.second + tup2.second) % M);
-}
-
-char Ndirection(int startN, int endN, int N) {
+char Ndirection(int startN, int endN) {
     int a = abs(startN - endN);
     int b = startN + N - endN;
     int c = endN + N - startN;
-    if (startN == endN) {
+    if (startN == endN)
         return '\0';
-    } else if (a < min(b, c)) {
+    else if (a < min(b, c))
         return (startN < endN) ? 'D' : 'U';
-    } else if (b < c) {
+    else if (b < c)
         return 'U';
-    } else {
+    else
         return 'D';
-    }
 }
 
-char Mdirection(int startM, int endM, int M) {
+char Mdirection(int startM, int endM) {
     int a = abs(startM - endM);
     int b = startM + M - endM;
     int c = endM + M - startM;
-    if (startM == endM) {
+    if (startM == endM)
         return '\0';
-    } else if (a < min(b, c)) {
+    else if (a < min(b, c))
         return (startM < endM) ? 'R' : 'L';
-    } else if (b < c) {
+    else if (b < c)
         return 'L';
-    } else {
+    else
         return 'L';
-    }
+}
+
+pair<int, int> add(pair<int, int> a, pair<int, int> b) {
+    return make_pair((a.first + b.first + N) % N, (a.second + b.second + M) % M);
 }
 
 int main() {
+    int T;
     cin >> T;
 
-    for (int t = 0; t < T; t++) {
-        int N, M;
+    map<char, pair<int, int>> moves = {
+        {'U', {-1, 0}},
+        {'D', {1, 0}},
+        {'R', {0, 1}},
+        {'L', {0, -1}}
+    };
+
+    for (int t = 0; t < T; ++t) {
         cin >> N >> M;
         string drift;
         cin >> drift;
         int driftLen = drift.length();
 
-        unordered_map<int, pair<int, int>> dic;
-        for (int n = 0; n < N; n++) {
-            for (int m = 0; m < M; m++) {
-                int x;
-                cin >> x;
-                dic[x] = make_pair(n, m);
+        map<int, pair<int, int>> dic;
+        for (int n = 0; n < N; ++n) {
+            for (int m = 0; m < M; ++m) {
+                int val;
+                cin >> val;
+                dic[val] = make_pair(n, m);
             }
         }
 
         pair<int, int> curr = make_pair(0, 0);
         int actions = 0;
         int driftCount = 0;
-        pair<int, int> goal = dic[1];
 
-        for (int i = 1; i <= N * M; i++) {
-            goal = dic[i];
+        for (int i = 1; i <= N * M; ++i) {
+            pair<int, int> goal = dic[i];
             while (goal != curr) {
                 char dr = drift[driftCount];
-                curr = add(curr, (dr == 'U') ? make_pair(-1, 0) : (dr == 'D') ? make_pair(1, 0) : (dr == 'R') ? make_pair(0, 1) : make_pair(0, -1), N, M);
+                curr = add(curr, moves[dr]);
                 driftCount = (driftCount + 1) % driftLen;
                 actions++;
-                if (goal == curr) {
+
+                if (goal == curr)
                     break;
-                } else {
-                    if (dr == 'R' || dr == 'L') {
-                        char d = Ndirection(curr.first, goal.first, N);
-                        if (d) {
-                            curr = add(curr, (d == 'D') ? make_pair(1, 0) : make_pair(-1, 0), N, M);
-                        } else {
-                            curr = add(curr, (dr == 'R') ? make_pair(0, 1) : make_pair(0, -1), N, M);
-                        }
-                    } else if (dr == 'U' || dr == 'D') {
-                        char d = Mdirection(curr.second, goal.second, M);
-                        if (d) {
-                            curr = add(curr, (d == 'U') ? make_pair(-1, 0) : make_pair(1, 0), N, M);
-                        } else {
-                            curr = add(curr, (dr == 'U') ? make_pair(-1, 0) : make_pair(1, 0), N, M);
-                        }
-                    }
+
+                if (dr == 'R' || dr == 'L') {
+                    char d = Ndirection(curr.first, goal.first);
+                    if (d)
+                        curr = add(curr, moves[d]);
+                    else
+                        curr = add(curr, moves[dr]);
+                } else if (dr == 'U' || dr == 'D') {
+                    char d = Mdirection(curr.second, goal.second);
+                    if (d)
+                        curr = add(curr, moves[d]);
+                    else
+                        curr = add(curr, moves[dr]);
                 }
             }
         }
+
         cout << actions << endl;
     }
+
     return 0;
 }
