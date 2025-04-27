10faf5c3a41359cf307acfe7113122c5f0493ea4
aarushth <65953492+aarushth@users.noreply.github.com>
2025-04-26 17:10:14 -0700

    Add files via upload

---
+++ 4_manifolds.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+def shortDistance(startN, startM, endN, endM):
+        return min(abs(startN-endN), min(startN+N-endN,endN+N-startN)) + min(abs(startM-endM), min(startM+M-endM, endM+M-startM))
+
+for t in range(T):
+    N, M = map(int, input().split())
+
+    
+
+    dic = {}
+    for n in range(N):
+        lis = [int(x) for x in input().split()]
+        for m in range(M):
+            dic[lis[m]] = (n, m)
+
+    prev = (0, 0)
+    tot = 0
+    for i in range(1, (N*M)+1):
+        tup = dic[i]
+        tot += shortDistance(prev[0], prev[1], tup[0], tup[1])
+        prev = tup
+        
+    print(tot)
